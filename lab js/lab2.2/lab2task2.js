a9b9580c537abee078cf0a1134d075d02b675ddf
Vasya13 <145290621+1nxiz@users.noreply.github.com>
2025-03-04 23:03:49 +0200

    Add files via upload

---
+++ lab js/lab2.2/lab2task2.js
@@ -0,0 +1,13 @@
+//Ex1
+let one ="";
+let two ="";
+let three ="3";
+let four ="";
+let five ="";
+console.log(one || two || three || four || five);
+
+//Ex2
+const hasApple = true;
+const hasJuise = false;
+console.log(!hasApple);
+console.log(!hasJuise);
